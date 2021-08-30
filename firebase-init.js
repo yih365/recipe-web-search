d57b457aa3fe8700e629829dcfd56624e06c563b
yih365 <68409635+yih365@users.noreply.github.com>
2021-08-29 21:28:49 -0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -2,12 +2,12 @@
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAk-7bPkiuRUGjz_8_-9hGjZ4w35-RyqUo",
-  authDomain: "recipe-manage-web.firebaseapp.com",
-  projectId: "recipe-manage-web",
-  storageBucket: "recipe-manage-web.appspot.com",
-  messagingSenderId: "74225911594",
-  appId: "1:74225911594:web:ce22c176702c4cb68d0053"
+  apiKey: "apiKey",
+  authDomain: "authDomain.firebaseapp.com",
+  projectId: "projectId",
+  storageBucket: "storageBucket.appspot.com",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId"
 };
 
 // Initialize Firebase
